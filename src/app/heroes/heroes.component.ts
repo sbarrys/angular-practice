7a713e4bb52778aae3b2ee6d1f6dce1d19bedb02
yun <rlaxodbs2015@gmail.com>
2020-07-07 13:16:36 +0900

    Observable<heroes>객체를 heroes객체로 읽어들임
    Observable<heroes>.subscribe( (heroes) = > (this.heroes = heroes));

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -15,7 +15,7 @@ export class HeroesComponent implements OnInit {
   // heroes = HEROES;
   heroes: Hero[];
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   selectedHero: Hero;
