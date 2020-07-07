a8698b12d8b99699e711344d4aaf2b620b0e48b0
yun <rlaxodbs2015@gmail.com>
2020-07-07 11:36:29 +0900

    http통신 ErrorHandling with Pipe

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,14 +5,21 @@ import { Observable } from 'rxjs';
 import { of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  //Mock 히어로 리스트 를 가져온다.
+  //http통신을 통해 데이터를 가져온다.
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
+
+  //Mock 히어로 리스트 를 가져온다
   // getHeroes(): Hero[] {
   //   return HEROES;
   // }
@@ -28,4 +35,18 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService
   ) {}
+
+  ////에러핸들링을/////
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
 }
