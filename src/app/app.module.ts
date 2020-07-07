acd4745d05ac95ef4517e6d9a71819335ec157d8
yun <rlaxodbs2015@gmail.com>
2020-07-07 15:37:22 +0900

    http통신경우 inMemoryApiService가 낚아챌수있게 셋팅
    이제 Heroes들은 인메모리에 있고, 서버로 부터 가져오듯이 api로 사용할 수 있다.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
   dataEncapsulation: false,
 });
